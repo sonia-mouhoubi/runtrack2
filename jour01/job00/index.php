b03383f1e4d779c7efd6ba027d987772b9b66566
M <sonia.mouhoubi@laplateforme.io>
2021-11-02 10:19:00 +0100

    Job 1 effectué

---
+++ jour01/job00/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Runtrack 2 Jour 1</title>        
+        <meta name="description" content="Premier jour de la Runtrack 2 PHP" />
+    </head>
+    
+    <body>
+        <?php
+            echo 'Hello LaPlateforme!'; //Ceci est un commentaire PHP
+        ?>
+    </body>
+</html>
