f8a9ca51db7a7be51416cd66113a5ac630765563
M <sonia.mouhoubi@laplateforme.io>
2021-11-02 11:36:17 +0100

    Commentaire sur une ligne et sur plusieurs ligne

---
+++ jour01/job02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Runtrack 2 Jour 1</title>        
+        <meta name="description" content="Premier jour de la Runtrack 2 PHP" />
+    </head>
+    
+    <body>
+        <?php
+            $str = "LaPlateforme";
+        
+
+            echo $str.'<br/>'; //Celi est un commentaire en une seule ligne
+            echo $str; /* Ceci est un commentaire sur
+                         plusieurs lignes */
+        ?>
+    </body>
+</html>
