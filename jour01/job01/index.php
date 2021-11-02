6b05eb96a8ce425228f5a50a3bd89f8908d1cdfe
M <sonia.mouhoubi@laplateforme.io>
2021-11-02 11:24:11 +0100

    VARIABLE VALEUR TRUE

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Runtrack 2 Jour 1</title>        
+        <meta name="description" content="Premier jour de la Runtrack 2 PHP" />
+    </head>
+    
+    <body>
+        <?php
+            $str = "LaPlateforme";
+            $str2 = "Vive";
+            $str3 = "!";
+            $val = 6 + 4;
+            $myBool = true;
+
+            echo $str.'<br/>';
+            echo $str2.$str.$str3.'<br/>';
+            echo $val.'<br/>';
+            echo $myBool;
+        ?>
+    </body>
+</html>
