bcf21a141327213e6c8720391d45c99e0e09d6fc
M <sonia.mouhoubi@laplateforme.io>
2021-11-03 15:21:34 +0100

    JOB 2

---
+++ jour02/job02/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Runtrack 2 Jour 1</title>        
+        <meta name="description" content="Premier jour de la Runtrack 2 PHP" />
+    </head>
+    
+    <body>
+        <?php
+          /*for ($i=0; $i <=1337 ; $i++) { 
+            echo $i;
+            echo "</br>";}  */ 
+           
+            for ($i=0; $i<=1337; $i++) { 
+
+                if ($i !=26 && $i !=37 && $i !=88 && $i !=1111) {
+                    echo $i;
+                    echo "</br>";
+                }
+            }
+
+        ?>
+        
+    </body>
+</html>
