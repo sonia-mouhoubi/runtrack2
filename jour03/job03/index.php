1c23374e9fe86222711a5175aaf58ce389dad5d5
M <sonia.mouhoubi@laplateforme.io>
2021-11-04 16:49:01 +0100

    Runtrack 2 JOUR 3 Job 3

---
+++ jour03/job03/index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+    $str = "I'm sorry Dave I'm afraid I can't
+    do that..";
+    $i = 0;
+    $a  = 'a';
+
+    while (isset($str[$i])) {
+        if ($str[$i]==$a) {
+            echo $str[$i];
+        }
+        $i++;
+
+    }*/
+    $tableau = ['a','e', 'i', 'o', 'u', 'y', 'A','E', 'I', 'O', 'U', 'Y']; 
+    $str ="I'm sorry Dave I'm afraid I can't
+    do that..";
+
+    //var_dump($tableau);
+    //print_r($tableau);
+        for ($i=0; isset($str[$i]);$i++) {
+            foreach ($tableau as $element) {
+                if ($str[$i] == $element) {
+                    echo $str[$i];
+                } 
+            }
+
+        }
+    
+    
+         
+?>
+ 
