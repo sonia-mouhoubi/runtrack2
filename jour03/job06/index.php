229f7af37a9383d7389055e2e51ce70951c27113
M <sonia.mouhoubi@laplateforme.io>
2021-11-04 22:38:13 +0100

    Runtrack 2 JOUR 3 JOB 6

---
+++ jour03/job06/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $str ='Les choses que l\'on possede finissent par nous posseder';
+    
+    for ($i=0; isset($str[$i]); $i--) {
+        echo $str[$i];
+        
+       
+ 
+    }
+   
+    
+?>
+ 
