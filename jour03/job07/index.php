4d4f20bd07ee6459e13eb98c9742aa2719c50c9b
M <sonia.mouhoubi@laplateforme.io>
2021-11-05 18:21:19 +0100

    Runtrack 2 Jour 3 JOB 7

---
+++ jour03/job07/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $str ='Certaines choses changent, et d\'autres
+    ne changeront jamais.';
+    
+    for ($i=1; isset($str[$i]); $i++) {
+        if($i %2 == 0);
+            
+           echo $str[$i];
+    } 
+    echo $str[0];
+?>
+ 
