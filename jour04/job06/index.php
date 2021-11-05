c4e458397a19d7d2fac343053a622b46f38a60ec
M <sonia.mouhoubi@laplateforme.io>
2021-11-05 18:25:55 +0100

    Runtrack 2 Jour 4 JOB 3

---
+++ jour04/job06/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+foreach($_GET as $element) {
+    if($element %2 == 0) {
+         echo $element." est paire </br>";
+    }
+    else {
+     echo $element." est impaire </br>";
+    }
+
+    var_dump ($_GET);
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Formulaire." />
+        <title>FORMULAIRE</title>
+    </head>
+    <body>
+        <main> 
+            <form action="" method="get">
+                    <input type="text" name="nombre" placeholder="Nombre">
+                    <button type="submit">Valider</button>
+            </form>  
+        </main>
+    </body>
+</html>
