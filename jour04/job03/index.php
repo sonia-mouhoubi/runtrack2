b21a98944172a742999afc7cf42d1a2755bf45d8
M <sonia.mouhoubi@laplateforme.io>
2021-11-05 18:24:11 +0100

    Runtrack 2 Jour 4 JOB 3

---
+++ jour04/job03/index.php
@@ -0,0 +1,60 @@
+<?php
+    $i = 0;
+
+    foreach ($_POST as $key => $value) {
+        $i++;
+    }
+    echo $i;
+    var_dump ($_POST);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Formulaire." />
+        <title>FORMULAIRE</title>
+    </head>
+    <body>
+        <header>
+            <h1>FORMULAIRE</h1>
+        </header>
+        <main> 
+            <form action="" method="post">
+                <legend>Créer un nouvel événement</legend>
+                <fieldset>
+                    <div class="cat">
+                        <label for="cat">Evénement</label>
+                        <input type="checkbox" id="cat" name="cat" value="">
+                    </div>
+                    <div>
+                        <label for="tache">Tâche</label>
+                        <input type="checkbox" id="cat" name="tache" value="">
+                    </div>
+                </fieldset> 
+                <fieldset>
+                    <div class="date">
+                        <label for="date">Date</label>
+                        <input type="date" id="date" name="date">
+                    </div>
+                    <div class="time">
+                        <label for="time">Heure</label>
+                        <input type="time" id="time" name="time">
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div class="titre">
+                        <label for="name">Titre</label> 
+                        <input value="" type="text" id="name" name="name" placeholder="Tapez votre titre">
+                    </div>
+                    <div class="description">
+                        <label for="msg">Description</label>
+                        <textarea id="msg" name="msg" placeholder="Tapez votre description"></textarea>
+                    </div>
+                </fieldset>
+                <button type="submit">Envoyer le message</button>
+            </form>  
+        </main>
+    </body>
+</html>
+ 
