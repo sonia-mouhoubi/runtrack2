02bec16f8b5d89414d9b59bab5274d1a5968cc64
M <sonia.mouhoubi@laplateforme.io>
2021-11-15 19:01:32 +0100

    Révision : JOUR 3 JOB 4. Compter et afficher le nb de caractère de $str

---
+++ jour06/jour03/job04/index.php
@@ -0,0 +1,10 @@
+<?php
+      $str ="“Dans l'espace, personne ne vous entend crier”.";
+    for($i=0; isset($str[$i]); $i++); //Parcourir et compter $str avec ISSET
+        echo $i; //Echo $i aprés incrémentation pour afficher le nb de caractère
+   
+     
+?>
+ 
+
+ 
