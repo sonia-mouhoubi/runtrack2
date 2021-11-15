9a9e915326f30dfda8fd45771255b8dce0ca360d
M <sonia.mouhoubi@laplateforme.io>
2021-11-15 13:45:48 +0100

    Révision : JOUR 2 JOB 3. Afficher de façons différentes les chiffres jusqu'à 100.

---
+++ jour06/jour02/job03/index.php
@@ -0,0 +1,18 @@
+<?php 
+for ($i=0; $i<=100; $i++) {
+    if($i>=0 && $i<=20) {
+        echo "<i>".$i."</i><br>";
+    }
+    elseif($i>=25 && $i<=50 && $i !=42) {
+        echo "<u>".$i."</u><br>";
+    }
+    elseif($i==42) {
+        echo "La Plateforme<br>";
+    }
+    else {
+        echo $i."<br>";
+    }
+}
+
+
+?>
