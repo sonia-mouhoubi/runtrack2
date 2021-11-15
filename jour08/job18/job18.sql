dc733af385fa0cc48b167e784d1fc2571212ad83
M <sonia.mouhoubi@laplateforme.io>
2021-11-15 16:47:48 +0100

    RUNTRACK 2 JOUR 9 JOB 18

--- jour08/job18/job18.sql
+++ jour08/job18/job18.sql
@@ -1,1 +1,1 @@
-SELECT salles.nom AS BiggestRoom, salles.capacite, etage.nom FROM `salles` INNER JOIN `etage` ORDER BY capacite DESC LIMIT 1
+SELECT salles.nom AS BiggestRoom, salles.capacite, etage.nom FROM `salles` INNER JOIN `etage` ON id_etage = etage.id ORDER BY capacite DESC LIMIT 1
