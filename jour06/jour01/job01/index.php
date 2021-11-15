8e8c9dc00ca10766cc43db5df6ba708c3a65e5b3
M <sonia.mouhoubi@laplateforme.io>
2021-11-15 10:30:04 +0100

    Runtrack 2 JOUR 1 Création de variable et echo de leur contenu

---
+++ jour06/jour01/job01/index.php
@@ -0,0 +1,19 @@
+<?php 
+$str = "LaPlateforme";
+echo $str.'<br/>';
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2.' '.$str.' '.$str3.'<br/>';
+
+$val = 6;
+echo $val.'<br/>';
+$val+=4;// Reviens à écrire $val = $val + 4
+echo $val.'<br/>';
+
+$myBool = TRUE;
+echo $myBool.'<br/>';
+$myBool = FALSE;
+echo $myBool.'<br/>';
+
+?>
